ead725307f8d84bdf2173d18d5e50d594eb75382
Amrendra Vikram Singh <76041208+AmrendraTheCoder@users.noreply.github.com>
2024-05-11 04:31:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <header className="text-center text-4xl font-bold mb-8 text-pink-800 bg-pink-200 rounded-lg p-4">
+      <header className="text-center text-2xl font-bold mb-8 text-pink-800 bg-pink-200 rounded-lg p-4">
       Hey Netra, even the bravest hearts get a little shaky sometimes. But remember, you are stronger than you know.
       </header>
       <header className="text-left text-xl font-bold mb-2 text-black rounded-lg p-4">
